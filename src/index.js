0a382aafda807d6046508bdf619d6eecfb0170f1
Mauricio Paternina <spaceinvadev@icloud.com>
2022-01-17 18:02:55 -0500

    refactor startApolloServer()

--- src/index.js
+++ src/index.js
@@ -3,20 +3,24 @@ const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
 const TrackAPI = require('./datasources/track-api')
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => {
-    return {
-      trackAPI: new TrackAPI()
+const startApolloServer = async (typeDefs, resolvers) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+      return {
+        trackAPI: new TrackAPI()
+      }
     }
-  }
-})
+  })
 
-const startApolloServer = async () => {
-  const { url } = await server.listen({ port: process.env.PORT || 4000 })
+  const { url, port } = await server.listen({ port: process.env.PORT || 4000 })
 
-  console.log(`🚀 Server ready at ${url}`)
+  console.log(`
+      🚀  Server is running
+      🔉  Listening on port ${port}
+      📭  Query at ${url}
+    `)
 }
 
-startApolloServer(server)
+startApolloServer(typeDefs, resolvers)
